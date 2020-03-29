b2d95b30d9cc2fe4409c433222825dfb656585cd
Mohammed Amr <haveagoodday.mido@gmail.com>
2020-03-29 22:58:18 +0200

    Encode Passwords. closes #20

--- src/main/java/com/flight/reservation/controllers/UserController.java
+++ src/main/java/com/flight/reservation/controllers/UserController.java
@@ -5,6 +5,8 @@ import com.flight.reservation.repos.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -17,6 +19,14 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepo;
+
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
 
@@ -40,6 +50,7 @@ public class UserController {
     @RequestMapping(value = "/registerUser", method = RequestMethod.POST)
     public String register(@ModelAttribute("user") User user) {
         LOGGER.info("Inside register() " + user);
+        user.setPassword(encoder.encode(user.getPassword()));
         userRepo.save(user);
         return "login/login";
     }
